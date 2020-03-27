688e86d114511b75af7f4f3ea9538d3b4ab83f2e
MLozyy <MLozovyywd@gmail.com>
2020-03-27 12:45:11 -0400

    Initial Commit

---
+++ simpleList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleList
+//
+//  Created by Mikhail Lozovyy on 3/27/20.
+//  Copyright © 2020 Mikhail Lozovyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
